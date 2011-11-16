a04fa8d81b5fee6cac881c9c88246eec8f7d0e20
Brandon Satrom <brsatrom@microsoft.com>
2011-11-16 15:44:31 -0600

    Added base Rails 3.1 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GemFodder::Application.config.secret_token = '80c1d74aa76e89113acadee262f9a715946398a51962798d9f2966d8e1c166f75ffc1e63c6f8b776d820e2d677c437feaf1448f832857d0bfa5ca6abf9de3034'
